37cd903fa02de9a35297dadcb7bd58c2bb6814d5
tkadjm <tkadjm@yahoo.com>
2017-01-01 17:48:01 -0600

    Inital files

---
+++ advocate-analysis-01.R
@@ -0,0 +1,74 @@
+## advocate analysis
+
+# *** SETUP ***
+rm(list=ls())
+# install.packages("data.table")
+# install.packages("doBy")
+# install.packages("plyr")
+install.packages("reshape")
+install.packages("reshape2")
+
+library(doBy)
+library(plyr)
+library(data.table)
+library(reshape2)
+library(reshape)
+
+setwd("/Users/thomaskeane/Documents/GitHub/iic")
+iic<-read.csv("iiccase.csv")
+
+plot(iic[iic$Code.Description=="Ablate Bone Tumor(s) Perq",]$Gross.Coll)
+plot(iic[iic$Code.Description=="X-ray Of Lower Spine Disk",]$Gross.Coll)
+
+cdata <- ddply(iic, c("Code.Description"), summarise,
+               N    = length(Code.Description),
+               average.charge = mean(Gross.Charges),
+               sd.charge = sd(Gross.Charges),
+               average.collection = mean(Gross.Coll),
+               sd.collection = sd(Gross.Coll),
+               tot = sum(Gross.Coll)
+)
+head(cdata)
+# write.csv(as.data.table(cdata), "cdata.csv")
+
+ddata <- ddply(iic, c("Doctor.Name.Variable"), summarise,
+               N    = length(Code.Description),
+               average.charge = mean(Gross.Charges),
+               sd.charge = sd(Gross.Charges),
+               average.collection = mean(Gross.Coll),
+               sd.collection = sd(Gross.Coll),
+               tot = sum(Gross.Coll),
+               rvus = sum(RVU)
+)
+head(ddata)
+# write.csv(as.data.table(ddata), "ddata.csv")
+
+sdata <- ddply(iic, c("Site.Name"), summarise,
+               N    = length(Code.Description),
+               average.charge = mean(Gross.Charges),
+               sd.charge = sd(Gross.Charges),
+               average.collection = mean(Gross.Coll),
+               sd.collection = sd(Gross.Coll),
+               collections = sum(Gross.Coll)
+)
+head(sdata)
+# write.csv(as.data.table(sdata), "sdata.csv")
+
+test01 <- ddply(iic, c("Site.Name", "Doctor.Name.Variable"), summarise,
+               N    = length(Code.Description),
+               average.charge = mean(Gross.Charges),
+               sd.charge = sd(Gross.Charges),
+               average.collection = mean(Gross.Coll),
+               sd.collection = sd(Gross.Coll),
+               collections = sum(Gross.Coll)
+)
+head(test01)
+write.csv(as.data.table(test01), "test01.csv")
+
+molten.iic<-melt(iic)
+test02<-cast(data=molten.iic, Site.Name~Doctor.Name.Variable)
+head(test02)
+head(test01)
+
+
+
